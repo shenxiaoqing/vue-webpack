534dd3dac136917d9f1eeca95595e035a8984561
shenxiaoqing <winnishen@qq.com>
2021-04-19 16:30:19 +0800

    remove

---
+++ 4.19webpack/test/src/index.js
@@ -0,0 +1,4 @@
+import './css/style.css'
+import img from './img/1.jpg';
+document.querySelector('img').src = img
+console.log(123456)
---
+++ 4.19webpack/test/webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        publicPath: '/dist'
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 8009,
+        // open: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(less|sass|scss|css)$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: {
+                    loader: 'url-loader',
+                    // options: {
+                    //     // 1024 = 1kb
+                    //     limit: 10240,
+                    //     name: 'assets/[name].[hash:6].[ext]',
+                    // }
+                }
+            },
+            {
+                test: /\.(eot|ttf|svg|woff2?)$/,
+                use: ['url-loader']
+            }
+        ]
+    },
+}
